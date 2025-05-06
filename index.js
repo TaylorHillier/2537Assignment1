a24250452d2841465826ca7f8104e7cfb11f696b
TaylorHillier <134565093+TaylorHillier@users.noreply.github.com>
2025-05-05 20:53:24 -0700

    added ejs templating and admin endpoint

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ const app = express();
 
 const Joi = require('joi');
 
-const expireTime = 1000 * 60 * 60 * 24; 
+const expireTime = 1000 * 60 * 60; // 1 hour
 
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 
 var {database} = require('./database.js');
 
 const userCollection = database.db(process.env.MONGODB_DATABASE).collection('users');
+
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
@@ -38,33 +40,18 @@ app.use(session({
 
 app.get('/', (req,res) => {
     if(req.session.username) {
-        res.send(`<h1> Hello ${req.session.username}!</h1><br><a href='/members'>Go to members section</a><br><a href='/logout'>Logout</a>
-            `);
+        res.render('homeLogged', {username: req.session.username});
     } else {
-        res.send("<br><a href='/signup'>Sign up</a><br><a href='/login'>Login</a><br>");
+        res.render('homeUnLogged');
     }
-    
 })
 
 app.get('/signup', (req,res) => {
-    res.send(
-        `<form action='/createUser' method='POST'>
-         <input type='text' name='username' placeholder='Enter a user name' required>
-            <input type='email' name='email' placeholder='Enter your email' required>
-             <input type='password' name='password' placeholder='Enter a Password' required>
-            <input type='submit' value='Submit'>
-        </form>`
-    )
+    res.render("signup");
 });
 
 app.get('/login', (req,res) => {
-    res.send(
-        `<form action='/loggingin' method='POST'>
-            <input type='email' name='email' placeholder='Enter your email' required>
-            <input type='password' name='password' placeholder='Enter a Password' required>
-            <input type='submit' value='Submit'>
-        </form>`
-    )
+    res.render("login");
 });
 
 app.get('/logout', (req,res) => {
@@ -85,20 +72,20 @@ app.post('/createUser', async (req,res) => {
 
     const validation = schema.validate(req.body);
     if(validation.error != null) {
-        res.send('Invalid username! SQL injection detected!');
+        res.render('sqlInjection');
         return;
     }
 
     const result = await userCollection.find({email: email}).toArray();
 
     if(result.length != 0) {
-        res.send('User already exists! <br><a href="/login">Login</a>');
+        res.render('duplicateUserError');
         return;
     }
 
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    await userCollection.insertOne({username: username, email: email, password: hashedPassword});
+    await userCollection.insertOne({username: username, email: email, password: hashedPassword, user_type: 'user'});
 
     req.session.authenticated = true;
     req.session.username = username;
@@ -108,6 +95,10 @@ app.post('/createUser', async (req,res) => {
 });
 
 app.post('/loggingin', async (req,res) => {
+
+    //initialize admin type for the my user
+    await userCollection.updateOne({username: 'taylorhillier'}, {$set: {user_type: 'admin'}});
+
     var email = req.body.email;
     var password = req.body.password;
 
@@ -118,13 +109,11 @@ app.post('/loggingin', async (req,res) => {
 
     const validation = schema.validate(req.body);
     if(validation.error != null) {
-        res.send('Invalid username! SQL injection detected!');
+        res.render('sqlInjection');
         return;
     }
     
-
     const result = await userCollection.find({email: email}).project({username: 1, email: 1, password: 1, _id: 1}).toArray();
-
 
     if(result.length != 1 ) {
         res.redirect('/signup');
@@ -142,7 +131,7 @@ app.post('/loggingin', async (req,res) => {
         return;
     }
     else {
-        res.send('Invalid password! <br><a href="/login">Login</a>');
+        res.render('passwordError');
         return;
     }
 });
@@ -151,21 +140,47 @@ app.get('/members', (req,res) => {
     let random = Math.ceil((Math.random()) * 3);
 
     if(req.session.authenticated) {
-        console.log("image" + random);
-        res.send(`<h1> Hello ${req.session.username}!</h1><br>
-            <br><img src="/image${random}.png" width='300px' height='200px'><br>
-            <a href='/logout'>Logout</a>`);
+        res.render('members', {username: req.session.username, image: random});
     } else {
         res.redirect('/');
     }
 }
 );
 
+app.get('/admin', async (req,res) => {
+
+    const users = await userCollection.find({}).toArray();
+
+    const userType = await userCollection.find({username: req.session.username}).project({user_type: 1}).toArray();
+    if(userType[0].user_type != 'admin') {
+        res.render('notAdminError');
+        return;
+    }
+    if(userType.length == 0) {
+        res.render('noUsersError');
+        return;
+    }
+
+    res.render('admin', { users });
+});
+
+app.post('/promoteUser', async (req,res) => {
+    const username = req.body.username;
+    await userCollection.updateOne({username: username}, {$set: {user_type: 'admin'}});
+    res.redirect('/admin');
+});
+
+app.post('/demoteUser', async (req,res) => {
+    const username = req.body.username;
+    await userCollection.updateOne({username: username}, {$set: {user_type: 'user'}});
+    res.redirect('/admin');
+});
+
 app.use((req,res) => {
     res.status(404).send('404 Not Found!');
 });
 
 app.listen(port, () => {
+    
     console.log(`Server is running on port ${port}`);
-
 });
