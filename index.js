2c022ec9782eb531ed6985f5239271520270339e
TaylorHillier <134565093+TaylorHillier@users.noreply.github.com>
2025-05-12 16:19:52 -0700

    added styling to 404

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ app.post('/demoteUser', isAuthenticated, isAdmin, async (req,res) => {
 });
 
 app.use((req,res) => {
-    res.status(404).send('404 Not Found!');
+    res.status(404).render('404');
 });
 
 app.listen(port, () => {
